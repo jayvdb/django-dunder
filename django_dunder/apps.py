4ed61db026220538fbbde3a8c24d19e7933ff40d
John Vandenberg <jayvdb@gmail.com>
2020-06-18 11:14:32 +0700

    Remove _PYTEST

--- django_dunder/apps.py
+++ django_dunder/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
-from .app_settings import _PYTEST, _ANY_REGISTER
+from .app_settings import _ANY_REGISTER
 
 
 class DunderConfig(AppConfig):
